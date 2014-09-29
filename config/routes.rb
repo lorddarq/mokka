c0f47f6caef9d874cabca2b5fd389d1a7d2ed878
Anton Platonov <platosha@gmail.com>
2014-09-29 18:23:54 +0400

    debug pages for tablet and mobile

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Mokka::Application.routes.draw do
   scope "mokka" do
     resources :pages
     get :widget, to: 'pages#widget'
-    get :debug, to: 'pages#debug'
     get :tablet, to: 'pages#tablet'
     get :phone, to: 'pages#phone'
+    get :debug, to: 'pages#debug'
+    get :debug_tablet, to: 'pages#debug_tablet'
+    get :debug_phone, to: 'pages#debug_phone'
     root "pages#index"
   end
 end
